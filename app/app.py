1616396e0611b979f4e2a80480b92c98438c87a4
ibalejandro <asanch41@eafit.edu.co>
2020-04-20 22:07:36 -0500

    Making verifications on the calculate breathing frequency request.

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.append('.')
+sys.path.append('../')
 
 import boto3
 import traceback
@@ -27,16 +27,42 @@ s3_client = boto3.client(
 
 @app.route("/calculate_bf", methods=["POST"])
 def calculate_bf():
-    body = request.json
-    audio_path = body.get("audio_path")
-    breathing_rate = Breathing_rate(audio_path)
-    breathing_frequency = breathing_rate.get_breathing_rate()
+    try:
+        if request.is_json:
+            try:
+                is_body_well_formed = True
+                body = request.json
+                audio_path = body["audio_path"]
+            except:
+                is_body_well_formed = False
+                response = {
+                    "message": "Specify a well-formed body."
+                }
+                status_code = 400
+            if is_body_well_formed:
+                breathing_rate = Breathing_rate(audio_path)
+                breathing_frequency = breathing_rate.get_breathing_rate()
+                response = {
+                    "audio_path": audio_path,
+                    "breathing_frequency": breathing_frequency["rate"]
+                }
+                status_code = 200
+        else:
+            response = {
+                "message": "Send a JSON request."
+            }
+            status_code = 400
+    except Exception as e:
+        response = {
+            "message": "Internal Server Error."
+        }
+        status_code = 500
+        print("****** Error processing audio file ******")
+        print(traceback.format_exc())
+        print("****** Error ******")
+        print(e)
 
-    response = {
-        "audio_path": audio_path,
-        "breathing_frequency": breathing_frequency.rate
-    }
-    return jsonify(response), 200
+    return jsonify(response), status_code
 
 
 @app.route("/health_check", methods=["GET"])
