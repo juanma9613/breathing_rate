6a6bf765e0dc974216b46efecc25335bd9377dde
ibalejandro <asanch41@eafit.edu.co>
2020-04-22 00:51:47 -0500

    Making a POST request to store the telemetry report.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 import boto3
 import traceback
+import requests
 from botocore.config import Config
 
 from flask import Flask, request, jsonify
@@ -38,6 +39,7 @@ def calculate_bf():
                 status_code = 400
             if is_body_well_formed:
                 try:
+                    original_audio_path = audio_path
                     audio_path = download_audio_from_s3(audio_path=audio_path)
                 except:
                     audio_path = None
@@ -50,6 +52,11 @@ def calculate_bf():
                         "breathing_frequency": breathing_frequency["rate"]
                     }
                     status_code = 200
+                    telemetry_report = {
+                        "breathing_frequency": breathing_frequency["rate"],
+                        "original_audio_path": original_audio_path
+                    }
+                    post_telemetry_report(telemetry_report=telemetry_report)
                 else:
                     response = {
                         "message": "Specify a valid audio path."
@@ -94,6 +101,49 @@ def download_audio_from_s3(audio_path):
         audio_file_name = ''
     audio_path = local_audio_path if audio_file_name != '' else None
     return audio_path
+
+
+def post_telemetry_report(telemetry_report):
+    token = get_auth_token()
+    body = {
+        "telemetry_type": "breathing_frequency",
+        "telemetry_report": {
+            "breathing_frequency": telemetry_report["breathing_frequency"]
+        },
+        "telemetry_source_id": telemetry_report["original_audio_path"]
+    }
+    try:
+        response = requests.post(url="https://reports-api-dev.5vid.co/api/v1/telemetry-reports/",
+                                 headers={"Content-Type": "application/json",
+                                          "Accept": "application/json",
+                                          "Authorization": f"Bearer {token}"},
+                                 json=body,
+                                 timeout=5)
+        status_code = response.status_code
+        if status_code != 200:
+            print("POST Result: {} - {}".format(str(status_code), response.reason))
+    except:
+        print("The telemetry report could not be posted.")
+
+
+def get_auth_token():
+    CLIENT_ID = os.getenv("CLIENT_ID")
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    AUDIENCE = os.getenv("AUDIENCE")
+    AUTH0_URL = os.getenv("AUTH0_URL")
+    try:
+        headers = {"Content-Type": "application/json"}
+        payload = {
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "audience": AUDIENCE,
+            "grant_type": "client_credentials"
+        }
+        response = requests.post(url=AUTH0_URL, json=payload, headers=headers)
+        return response.json()["access_token"]
+    except Exception as error:
+        print("An error occurred getting Auth Token.")
+        print(error)
 
 
 if __name__ == "__main__":
